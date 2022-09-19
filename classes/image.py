8848e1b7ded6630877799587f462a76b1efd6e32
Bryzgalov.Pavel <Bryzgalov.Pavel@sogaz.ru>
2022-09-19 15:13:52 +0300

    Temp

--- classes/image.py
+++ classes/image.py
@@ -1,9 +1,13 @@
 import cv2
+import numpy
+
+from classes.сounter import Counter
 
 
-class Image():
+class Image:
     def __init__(self, file_path: str):
         self.image = cv2.imread(file_path, cv2.IMREAD_COLOR)
+        self.contrasted_image = None
         # self.resize()
 
     def resize(self, scale: float = 0.5):
@@ -36,3 +40,53 @@ class Image():
         rotation_matrix[1, 2] += bound_h / 2 - center[1]
 
         rotated = cv2.warpAffine(image, rotation_matrix, (bound_w, bound_h))
+
+    def crop_image(self, counter: Counter):
+        crop = self.image[counter.top_left[1]: counter.top_left[1] + counter.get_height(),
+                          counter.top_left[0]: counter.top_left[0] + counter.get_wight()]
+        return crop
+
+    def contrast_image(self):
+        """
+        :param image: получает массив цветов каждого пикселя
+        :return: преобразованный массив цветов каждого пикселя с улучшенной контрастностью
+        """
+        # разделяем массив для получения контрастности пикселей
+        lab = cv2.cvtColor(self.image, cv2.COLOR_BGR2LAB)
+        l, a, b = cv2.split(lab)
+        # увеличиваем контрастность кадого пикселя
+        clahe = cv2.createCLAHE(clipLimit=7.0, tileGridSize=(8, 8))
+        cl = clahe.apply(l)
+        # объединяем назад в один массив
+        limg = cv2.merge((cl, a, b))
+        self.contrasted_image = cv2.cvtColor(limg, cv2.COLOR_Lab2BGR)
+
+    def find_counters(self, image, standart_image):
+        all_counters = []
+        (height, weight) = image.shape[:2]
+        image_contrast = cv2.resize(image, (image.shape[1], image.shape[0]))
+        # преобразовывем изображение в оттенки серого
+        gray = cv2.cvtColor(image_contrast, cv2.COLOR_BGR2GRAY)
+        # производим бинаризацию
+        thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+        counters, hi = cv2.findContours(thresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        image_counter = cv2.drawContours(image_contrast, counters, -1, (0, 0, 255), 1, cv2.LINE_8, hierarchy=hi)
+        for counter in counters:
+            x, y, w, h = cv2.boundingRect(counter)
+            rect = cv2.minAreaRect(counter)  # пытаемся вписать прямоугольник
+            box = cv2.boxPoints(rect)  # поиск четырех вершин прямоугольника
+            box = np.int0(box)  # округление координат
+            my_data = np.sort(box, axis=0)
+            for i in [my_data]:
+                if abs(my_data[2][1] - my_data[0][1]) >= 20 and abs(
+                        my_data[2][0] - my_data[0][0]) >= 40 and w < weight - 50 and h < height - 50:
+                    all_counters.append(Counter(i, w, h, standart_image, image))
+
+        our_counters = []
+        for i in all_counters:
+            for j in all_counters:
+                i.is_inside(j)
+            if i.count_inner == 0:
+                our_counters.append(i)
+
+        return our_counters
--- classes/сounter.py
+++ classes/сounter.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class Counter():
+class Counter:
     def __init__(self, box):
         self.top_left = (box[0][0], box[0][1])
         self.top_right = (box[1][0], box[1][1])
@@ -17,46 +17,6 @@ class Counter():
 
         self.count_inner = 0
 
-    def __repr__(self):
-        return f"{self.top_left_y}|{self.column}"
-
-    def __lt__(self, other):
-        return self.top_left_y < other.top_left_y
-
-    def __le__(self, other):
-        return self.top_left_y <= other.top_left_y
-
-    def __gt__(self, other):
-        return self.top_left_y > other.top_left_y
-
-    def __ge__(self, other):
-        return self.top_left_y >= other.top_left_y
-
-    def __eq__(self, other):
-        return self.top_left_y == other.top_left_y
-
-    def __ne__(self, other):
-        return self.top_left_y != other.top_left_y
-
-    def is_inside(self, other):
-        if (self.top_left_x > other.top_left_x) and (self.top_left_y + 2 > other.top_left_y) and (
-                self.down_right_x < other.down_right_x) and (self.down_right_y - 2 < other.down_right_y) or (
-                self.top_right_x < other.top_right_x) and (self.top_right_y + 2 > other.top_right_y) and (
-                self.down_left_x > other.down_left_x) and (self.down_left_y - 2 < other.down_left_y):
-            self.count_inner += 1
-
-    def crop_image(self):
-        crop = self.standart_image[self.top_left_y: self.top_left_y + self.h,
-               self.top_left_x: self.top_left_x + self.w]
-        return crop
-
-    def coordinares(self):
-        coordinate_array = np.array([[int(self.top_left_x), int(self.top_left_y)],
-                                     [int(self.top_right_x), int(self.top_right_y)],
-                                     [int(self.down_right_x), int(self.down_right_y)],
-                                     [int(self.down_left_x), int(self.down_left_y)]])
-        return coordinate_array
-
     def get_wight(self):
         return max(self.top_left[0], self.top_right[0], self.down_left[0], self.down_right[0]) - \
                min(self.top_left[0], self.top_right[0], self.down_left[0], self.down_right[0])
@@ -64,3 +24,40 @@ class Counter():
     def get_height(self):
         return max(self.top_left[1], self.top_right[1], self.down_left[1], self.down_right[1]) - \
                min(self.top_left[1], self.top_right[1], self.down_left[1], self.down_right[1])
+
+    def __repr__(self) -> str:
+        return f"{self.top_left[1]}|{self.column}"
+
+    def __lt__(self, other) -> bool:
+        return self.top_left[1] < other.top_left[1]
+
+    def __le__(self, other) -> bool:
+        return self.top_left[1] <= other.top_left[1]
+
+    def __gt__(self, other) -> bool:
+        return self.top_left[1] > other.top_left[1]
+
+    def __ge__(self, other) -> bool:
+        return self.top_left[1] >= other.top_left[1]
+
+    def __eq__(self, other) -> bool:
+        return self.top_left == other.top_left and self.top_right == other.top_right and \
+               self.down_left == other.down_left and self.down_right == other.down_right
+
+    def __ne__(self, other):
+        return self.top_left[1] != other.top_left[1]
+
+    def is_inside(self, other):
+        if (self.top_left[0] > other.top_left[0]) and (self.top_left[1] + 2 > other.top_left[1]) and (
+                self.down_right[0] < other.down_right[0]) and (self.down_right[1] - 2 < other.down_right[1]) or (
+                self.top_right[0] < other.top_right[0]) and (self.top_right[1] + 2 > other.top_right[1]) and (
+                self.down_left[0] > other.down_left[0]) and (self.down_left[1] - 2 < other.down_left[1]):
+            self.count_inner += 1
+
+
+    def coordinares(self):
+        coordinate_array = np.array([[int(self.top_left[0]), int(self.top_left[1])],
+                                     [int(self.top_right[0]), int(self.top_right[1])],
+                                     [int(self.down_right[0]), int(self.down_right[1])],
+                                     [int(self.down_left[0]), int(self.down_left[1])]])
+        return coordinate_array
