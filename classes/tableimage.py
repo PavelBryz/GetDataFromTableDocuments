6627a0c570fe71aefde17763c5e7d57bcd901cd5
Bryzgalov.Pavel <Bryzgalov.Pavel@sogaz.ru>
2022-10-03 17:49:46 +0300

    Add comments

--- classes/tableimage.py
+++ classes/tableimage.py
@@ -27,29 +27,26 @@ class TableImage(Image):
             box = np.int0(box)  # округление координат
             box = np.sort(box, axis=0)
 
-            sm = cv2.arcLength(counter, True)
-
-            # if box[0][0] == 0 or box[0][1] == 0 or w <= 5 or h <= 5: continue
-            # # if abs(box[0][1] - box[1][1]) < 10 : continue
-            # if box[0][0] < 0 or box[0][1] < 0 or \
-            #    box[1][0] < 0 or box[1][1] < 0 or \
-            #    box[2][0] < 0 or box[2][1] < 0 or \
-            #    box[3][0] < 0 or box[3][1] < 0: continue
+            # Filter values less than zero
+            if box[0][0] < 0 or box[0][1] < 0 or \
+               box[1][0] < 0 or box[1][1] < 0 or \
+               box[2][0] < 0 or box[2][1] < 0 or \
+               box[3][0] < 0 or box[3][1] < 0: continue
 
             box_width = Contour.get_wight_static(box)
             box_height = Contour.get_height_static(box)
 
+            # Filter box if it's size close ot the size of table
             if (box[0][0] + box[0][1]) < 25 and (box_width + box_height > (height + width - 25)): continue
 
-            # if sm > (height * width * 2 * 0.9): continue
-
+            # Filter box if it's size less than 0,3% of table or perimeter less than 255. 255 - magic number
+            # ToDo think what to do with 255
+            sm = cv2.arcLength(counter, True)
             if (box_width * box_height) < (height * width / 300) or sm <= 225: continue
-            # if area < (height * width / 100) or sm <= 225: continue
 
-            # if sm <= 225 or len(approx) != 4: continue
-            # if (1 < abs(box[2][1] - box[0][1]) < 24) or (1 < abs(box[2][0] - box[0][0]) < 80): continue
             self.cells.append(ContourOfCell(box))
 
+        # If none contours was found it means that image its self is a cell, so we add it
         if len(self.cells) == 0:
             box = [[0, 0],
                    [width, 0],
